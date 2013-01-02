7a603113ad5525fe4b34af184238a1ca65df1047
Harry <hjwp2@cantab.net>
2013-01-02 15:20:36 +0100

    First crack at chapter 2, testing exceptions.

--- book0/rome.py
+++ book0/rome.py
@@ -1,2 +1,8 @@
 def add(augend, addend):
-    return augend + addend
+    if not isinstance(augend, basestring) or not isinstance(addend, basestring):
+        raise ValueError
+    simple_sum = augend + addend
+    if any(char != 'I' for char in simple_sum):
+        raise ValueError
+    return simple_sum
+
--- book0/tests.py
+++ book0/tests.py
@@ -3,15 +3,22 @@ from rome import add
 
 class AdditionTest(unittest.TestCase):
 
-    def test_adding_iis(self):
+    def test_adding_Is(self):
         self.assertEqual(add('I', 'I'), 'II')
         self.assertEqual(add('I', 'II'), 'III')
 
-    def test_v_and_iv(self):
-        self.assertEqual(add('II', 'III'), 'V')
-        self.assertEqual(add('V', 'I'), 'VI')
-        self.assertEqual(add('II', 'II'), 'IV')
-        self.assertEqual(add('IV', 'I'), 'V')
+
+    def test_inputs_out_of_scope_raise_exceptions(self):
+        for bad_input in (2, None, 'Z', 'V', 'X', 'L', 'C', 'D', 'M'):
+            with self.assertRaises(ValueError) as m:
+                add('I', bad_input)
+                if not hasattr(m, 'exception'):
+                    self.fail('%s as augend did not raise exception' % bad_input)
+
+            with self.assertRaises(ValueError) as m:
+                add(bad_input, 'I')
+                if not hasattr(m, 'exception'):
+                    self.fail('%s as addend did not raise exception' % bad_input)
 
 
 if __name__ == '__main__':
