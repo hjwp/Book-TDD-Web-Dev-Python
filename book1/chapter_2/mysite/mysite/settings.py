d17618bb352d5fda7196a781b55232be08cf002f
Harry <hjwp2@cantab.net>
2013-02-09 16:59:56 +0000

    Start again on chap 2. Restore mysite from chapter 1.

--- book1/chapter_2/mysite/mysite/settings.py
+++ book1/chapter_2/mysite/mysite/settings.py
@@ -119,7 +119,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'fts',
 )
 
 # A sample logging configuration. The only tangible logging
