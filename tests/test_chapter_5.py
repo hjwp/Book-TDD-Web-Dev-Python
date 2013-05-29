200ec1b462e005f39a80c574f12c75d6396e7034
Harry <hjwp2@cantab.net>
2013-05-29 09:43:21 +0100

    tests: start on chapter 5

---
+++ tests/test_chapter_5.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from book_tester import (
+    ChapterTest,
+    CodeListing,
+    Command,
+    Output,
+    parsed_html,
+    parse_listing,
+)
+
+class Chapter5Test(ChapterTest):
+
+    def test_listings_and_commands_and_output(self):
+        chapter_5 = parsed_html.cssselect('div.sect1')[5]
+        listings_nodes = chapter_5.cssselect('div.listingblock')
+        self.listings = [p for n in listings_nodes for p in parse_listing(n)]
+
+        # sanity checks
+        self.assertEqual(type(self.listings[0]), CodeListing)
+        self.assertEqual(type(self.listings[1]), Command)
+        self.assertEqual(type(self.listings[2]), Output)
+
+        self.start_with_checkout(5)
+        self.start_dev_server()
+
+        while self.pos < 100:
+            self.recognise_listing_and_process_it()
+
+        self.check_final_diff(5)
+        self.assert_all_listings_checked(self.listings)
+
+
+if __name__ == '__main__':
+    unittest.main()
