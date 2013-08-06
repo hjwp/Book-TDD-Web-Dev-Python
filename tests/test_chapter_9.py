82f845065eb513949184a660eb1f4aac6266f69c
Harry <hjwp2@cantab.net>
2013-08-06 11:52:32 +0100

    New start on chapter 9 yay!

---
+++ tests/test_chapter_9.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import unittest
+import time
+
+from book_tester import ChapterTest
+
+class Chapter9Test(ChapterTest):
+    chapter_no = 9
+
+    def test_listings_and_commands_and_output(self):
+        self.parse_listings()
+
+        # sanity checks
+        print(self.listings[0])
+        self.assertEqual(self.listings[0].type, 'code listing with git ref')
+        self.assertEqual(self.listings[1].type, 'other command')
+
+        self.sourcetree.start_with_checkout(self.chapter_no)
+        # other prep
+        self.sourcetree.run_command('mkdir ../database')
+        self.sourcetree.run_command('python3 manage.py syncdb --noinput')
+
+        # skips
+        #self.listings[4].skip = True # whatever
+
+        while self.pos < 200:
+            print(self.pos)
+            self.recognise_listing_and_process_it()
+
+        self.assert_all_listings_checked(self.listings)
+        self.check_final_diff(self.chapter_no)
+
+
+if __name__ == '__main__':
+    unittest.main()
