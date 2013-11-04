12fc5a7cae81c065d094f305113d32e9e53d73f2
Harry <hjwp2@cantab.net>
2013-11-04 17:15:12 +0000

    updates to makefile, script to update sources for tests

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-SOURCES := $(wildcard *.asciidoc)
+SOURCES := $(wildcard chapter_*.asciidoc)
+CHAPTER_TESTS := $(wildcard tests/test_chapter_*.py)
 HTML_PAGES := $(patsubst %.asciidoc, %.html, ${SOURCES})
 RUN_ASCIIDOC = python2.7 `which asciidoc` 
 
@@ -8,12 +9,14 @@ book.html: $(SOURCES)
 build: $(HTML_PAGES)
 
 test: build
+	./update_source_repo.py
 	./run_all_tests.sh
 
 %.html: %.asciidoc
 	$(RUN_ASCIIDOC) $<
 
 test_chapter_%: chapter_%.html
+	./update_source_repo.py $(subst test_chapter_,,$@)
 	py.test -s ./tests/$@.py
 
 clean:
---
+++ update_source_repo.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Update required checkouts in source repo (branch for chap. n-1)
+
+Usage:
+  update_source_repo.py [<chapter_no>]
+
+Options:
+  -h --help                                 Show this screen.
+
+"""
+from docopt import docopt
+import subprocess
+import os
+
+THIS_FOLDER = os.path.abspath(os.path.dirname(__file__))
+
+
+def update_sources_for_chapter(chapter_no):
+    target = os.path.join(
+        THIS_FOLDER, 'source', 'chapter_{0:02d}'.format(chapter_no), 'superlists'
+    )
+    print('updating', target)
+    chapter_before = 'chapter_{0:02d}'.format(chapter_no - 1)
+    current_commit = subprocess.check_output(['git', 'log', '-n 1', '--format=%H'], cwd=target).decode().strip()
+    subprocess.check_output(['git', 'fetch'], cwd=target)
+    print('current commit', current_commit)
+    if chapter_no > 1:
+        subprocess.check_output(['git', 'checkout', chapter_before], cwd=target)
+        subprocess.check_output(['git', 'reset', '--hard', 'origin/{}'.format(chapter_before)], cwd=target)
+        subprocess.check_output(['git', 'checkout', current_commit], cwd=target)
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    if arguments['<chapter_no>']:
+        update_sources_for_chapter(int(arguments['<chapter_no>']))
+    else:
+        print('updating all source repos')
+        folders = os.listdir(os.path.join(THIS_FOLDER, 'source'))
+        for folder in sorted(folders):
+            if folder.startswith('chapter_'):
+                chapter_no = int(folder[8:10])
+                update_sources_for_chapter(chapter_no)
+
+
+
