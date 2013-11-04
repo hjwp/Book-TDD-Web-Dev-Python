d8328b089d75bb2aa056992eda3e5f70dc4593bb
Harry <hjwp2@cantab.net>
2013-11-04 18:17:19 +0000

    update chapter to branch rather than commit specified in gitmodule

--- update_source_repo.py
+++ update_source_repo.py
@@ -21,14 +21,15 @@ def update_sources_for_chapter(chapter_no):
     )
     print('updating', target)
     subprocess.check_output(['git', 'submodule', 'update', target])
+    current_chapter = 'chapter_{0:02d}'.format(chapter_no)
     chapter_before = 'chapter_{0:02d}'.format(chapter_no - 1)
-    current_commit = subprocess.check_output(['git', 'log', '-n 1', '--format=%H'], cwd=target).decode().strip()
+    #current_commit = subprocess.check_output(['git', 'log', '-n 1', '--format=%H'], cwd=target).decode().strip()
     subprocess.check_output(['git', 'fetch'], cwd=target)
-    print('current commit', current_commit)
+    #print('current commit', current_commit)
     if chapter_no > 1:
         subprocess.check_output(['git', 'checkout', chapter_before], cwd=target)
         subprocess.check_output(['git', 'reset', '--hard', 'origin/{}'.format(chapter_before)], cwd=target)
-        subprocess.check_output(['git', 'checkout', current_commit], cwd=target)
+        subprocess.check_output(['git', 'checkout', current_chapter], cwd=target)
 
 
 if __name__ == '__main__':
