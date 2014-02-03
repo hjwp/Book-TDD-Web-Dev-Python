f801383ff04142d1ce765256370491a5a1d9982e
Harry <hjwp2@cantab.net>
2014-02-03 02:35:39 +0000

    12 now passing

--- tests/test_chapter_12.py
+++ tests/test_chapter_12.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+# -*- coding: utf-8 -*-
 import unittest
 
 from book_tester import ChapterTest
@@ -11,16 +11,12 @@ class Chapter12Test(ChapterTest):
         self.parse_listings()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'other command')
-        self.assertEqual(self.listings[1].type, 'output')
-        self.assertEqual(self.listings[2].type, 'code listing with git ref')
-        self.skip_with_check(47, 'needs the -f')
-        self.skip_with_check(50, 'git push -f origin')
-        fab_pos = 36
-        assert 'fab' in self.listings[fab_pos]
+        self.assertEqual(self.listings[0].type, 'code listing with git ref')
+        self.assertEqual(self.listings[1].type, 'test')
+        self.assertEqual(self.listings[2].type, 'output')
 
         self.sourcetree.start_with_checkout(self.chapter_no)
-        self.prep_virtualenv()
+        # other prep
         self.sourcetree.run_command('mkdir ../database')
         self.sourcetree.run_command('python3 manage.py syncdb --noinput')
 
@@ -30,22 +26,11 @@ class Chapter12Test(ChapterTest):
         # hack fast-forward
         skip = False
         if skip:
-            self.pos = 37
+            self.pos = 38
             self.sourcetree.run_command('git checkout {0}'.format(
                 self.sourcetree.get_commit_spec('ch12l015')
             ))
 
-        # while self.pos < fab_pos:
-        #     print(self.pos)
-        #     self.recognise_listing_and_process_it()
-
-        # self.sourcetree.run_command('git stash')
-        # self.sourcetree.run_command('git checkout repo/chapter_12^')
-        # self.sourcetree.run_command('git stash pop')
-        # self.recognise_listing_and_process_it()
-        # self.sourcetree.run_command('git stash')
-        # self.sourcetree.run_command('git checkout master')
-        # self.sourcetree.run_command('git stash pop')
 
         while self.pos < len(self.listings):
             print(self.pos)
