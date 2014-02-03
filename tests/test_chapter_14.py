27cf1f0b55718624790f3e4006460a82ba9702b2
Harry <hjwp2@cantab.net>
2014-02-03 02:54:19 +0000

    feed thru changes to 14 (js)

--- tests/test_chapter_14.py
+++ tests/test_chapter_14.py
@@ -11,22 +11,19 @@ class Chapter14Test(ChapterTest):
         self.sourcetree.start_with_checkout(self.chapter_no)
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing with git ref')
+        self.assertEqual(self.listings[0].type, 'code listing')
         self.assertEqual(self.listings[1].type, 'code listing with git ref')
-        self.assertEqual(self.listings[2].type, 'other command')
-        self.assertTrue(self.listings[87].dofirst)
+        self.assertEqual(self.listings[2].type, 'code listing with git ref')
 
         # skips
-        self.skip_with_check(22, 'switch back to master') # comment
-        self.skip_with_check(24, 'remove any trace') # comment
-        self.skip_with_check(26, 'new FT') # comment
+        #self.skip_with_check(30, '# review changes') # diff
 
         # hack fast-forward
         skip = False
         if skip:
-            self.pos = 46
+            self.pos = 5
             self.sourcetree.run_command('git checkout {0}'.format(
-                self.sourcetree.get_commit_spec('ch14l024')
+                self.sourcetree.get_commit_spec('ch13l004')
             ))
 
         while self.pos < len(self.listings):
@@ -34,14 +31,6 @@ class Chapter14Test(ChapterTest):
             self.recognise_listing_and_process_it()
 
         self.assert_all_listings_checked(self.listings)
-        # fix incomplete moves from dofirst-ch14l019
-        self.sourcetree.run_command('git rm lists/static/base.css')
-        self.sourcetree.run_command('git rm -r lists/static/bootstrap')
-        self.sourcetree.run_command('git rm lists/static/tests/qunit.css')
-        self.sourcetree.run_command('git rm lists/static/tests/qunit.js')
-
-        # and from the diff-version of settings.py
-        self.sourcetree.run_command('rm superlists/settings.py.orig')
         self.sourcetree.run_command('git add . && git commit -m"final commit"')
         self.check_final_diff(self.chapter_no)
 
