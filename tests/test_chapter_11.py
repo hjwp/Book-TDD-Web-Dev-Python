1efe0a4b182793e1ead69e038b5c56471a980d7b
Harry <hjwp2@cantab.net>
2014-02-03 01:41:40 +0000

    No. 11 is now consistent, if not reviewed

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -11,8 +11,8 @@ class Chapter11Test(ChapterTest):
         self.parse_listings()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing with git ref')
-        self.assertEqual(self.listings[1].type, 'test')
+        self.assertEqual(self.listings[0].type, 'code listing')
+        self.assertEqual(self.listings[1].type, 'code listing')
         self.assertEqual(self.listings[2].type, 'output')
 
         self.sourcetree.start_with_checkout(self.chapter_no)
@@ -21,14 +21,14 @@ class Chapter11Test(ChapterTest):
         self.sourcetree.run_command('python3 manage.py syncdb --noinput')
 
         # skips
-        #self.skip_with_check(30, '# review changes') # diff
+        self.skip_with_check(31, '# review changes') # diff
 
         # hack fast-forward
         skip = False
         if skip:
-            self.pos = 37
+            self.pos = 50
             self.sourcetree.run_command('git checkout {0}'.format(
-                self.sourcetree.get_commit_spec('ch11l015')
+                self.sourcetree.get_commit_spec('ch11l023')
             ))
 
 
