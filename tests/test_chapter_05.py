fa1b9bba420d5c7434c4e25c16ff5f62681b79b4
Harry <hjwp2@cantab.net>
2014-03-26 14:24:56 +0000

    try to fix intermittent bug in 5

--- tests/test_chapter_05.py
+++ tests/test_chapter_05.py
@@ -14,6 +14,7 @@ class Chapter5Test(ChapterTest):
     chapter_no = 5
 
     def restart_dev_server(self):
+        print('restarting dev server')
         self.run_command(Command('pkill -f runserver'))
         time.sleep(1)
         self.start_dev_server()
@@ -27,9 +28,13 @@ class Chapter5Test(ChapterTest):
         self.assertEqual(type(self.listings[0]), CodeListing)
         self.assertEqual(type(self.listings[1]), Command)
         self.assertEqual(type(self.listings[2]), Output)
+
         syncdb_pos = 66
         assert 'syncdb' in self.listings[syncdb_pos]
         assert self.listings[syncdb_pos].type == 'interactive manage.py'
+
+        nutemplate_pos = 62
+        assert "{'items': items}" in self.listings[nutemplate_pos].contents
 
         self.sourcetree.start_with_checkout(5)
         self.start_dev_server()
@@ -37,12 +42,16 @@ class Chapter5Test(ChapterTest):
         # skips
         self.skip_with_check(73, "3: Buy peacock feathers")
 
-        dev_server_restarted = False
+        restarted_after_syncdb = False
+        restarted_after_nutemplate = False
         while self.pos < len(self.listings):
             print(self.pos)
-            if self.pos > syncdb_pos and not dev_server_restarted:
+            if self.pos > syncdb_pos and not restarted_after_syncdb:
                 self.restart_dev_server()
-                dev_server_restarted = True
+                restarted_after_syncdb = True
+            if self.pos > nutemplate_pos and not restarted_after_nutemplate:
+                self.restart_dev_server()
+                restarted_after_nutemplate = True
             self.recognise_listing_and_process_it()
 
         self.assert_all_listings_checked(self.listings)
