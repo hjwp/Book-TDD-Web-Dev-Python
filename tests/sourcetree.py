0ef513c0e85183ab5d4ac8c7022042ce2f89b399
Harry <hjwp2@cantab.net>
2016-03-08 23:19:59 +0000

    try git show -p instead of -M to try and fix patch issue in 14

--- tests/sourcetree.py
+++ tests/sourcetree.py
@@ -163,7 +163,7 @@ class SourceTree(object):
         commit_spec = self.get_commit_spec(commit_ref)
         self.run_command(
             #'git diff {commit}^ {commit} | patch'.format(commit=commit_spec)
-            'git show -M {commit} | patch -p1 --fuzz=3 --no-backup-if-mismatch'.format(commit=commit_spec)
+            'git show -p {commit} | patch -p1 --fuzz=3 --no-backup-if-mismatch'.format(commit=commit_spec)
         )
         #self.run_command('git reset')
 
