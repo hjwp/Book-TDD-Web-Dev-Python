d4c0a744209b78c09f6d174bc6321be6e5890483
Harry <hjwp2@cantab.net>
2016-10-13 10:02:51 +0100

    update tag check in 19 test

--- tests/test_chapter_19.py
+++ tests/test_chapter_19.py
@@ -42,7 +42,7 @@ class Chapter19Test(ChapterTest):
             if check_head:
                 print('checking chapter started from correct tag')
                 revision = self.sourcetree.run_command('git rev-parse HEAD')
-                assert "a697785" in revision
+                assert "f8d00f6" in revision
                 print('OK')
 
         self.assert_all_listings_checked(self.listings)
