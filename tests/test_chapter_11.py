ae58d6ba2df98e7724b5b30281882afbe9309cfa
Harry <hjwp2@cantab.net>
2016-12-04 14:44:35 +0000

    11 works, with new html5 validation gubbins

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -23,7 +23,7 @@ class Chapter11Test(ChapterTest):
         self.prep_database()
 
         # hack fast-forward
-        skip = True
+        skip = False
         if skip:
             self.pos = 59
             self.sourcetree.run_command('git checkout {}'.format(
