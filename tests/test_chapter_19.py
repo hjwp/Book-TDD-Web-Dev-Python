a582f669f2363e26f39e7313b44391932ab6b81c
Harry <hjwp2@cantab.net>
2016-12-06 11:31:42 +0000

    feed thru requirements.txt to all chapters

--- tests/test_chapter_19.py
+++ tests/test_chapter_19.py
@@ -42,7 +42,7 @@ class Chapter19Test(ChapterTest):
             if check_head:
                 print('checking chapter started from correct tag')
                 revision = self.sourcetree.run_command('git rev-parse HEAD')
-                assert "cd567de" in revision
+                assert "995b6cb" in revision
                 print('OK')
 
         self.assert_all_listings_checked(self.listings)
