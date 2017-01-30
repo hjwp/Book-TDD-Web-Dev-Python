56466ce072705b33435b53ac0aae0411637c44ca
Harry <hjwp2@cantab.net>
2017-01-30 01:34:26 +0000

    fixup our 19

--- tests/test_chapter_19.py
+++ tests/test_chapter_19.py
@@ -4,17 +4,17 @@ import unittest
 from book_tester import ChapterTest
 
 
-class Chapter18Test(ChapterTest):
-    chapter_no = 18
+class Chapter19Test(ChapterTest):
+    chapter_no = 19
 
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
         #self.prep_virtualenv()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing')
-        self.assertEqual(self.listings[1].type, 'output')
-        self.assertEqual(self.listings[2].type, 'code listing with git ref')
+        self.assertEqual(self.listings[0].type, 'code listing with git ref')
+        self.assertEqual(self.listings[1].type, 'code listing with git ref')
+        self.assertEqual(self.listings[2].type, 'code listing currentcontents')
 
         # skips
         #self.skip_with_check(22, 'switch back to master') # comment
@@ -23,11 +23,11 @@ class Chapter18Test(ChapterTest):
         self.prep_database()
 
         # hack fast-forward
-        skip = False
+        skip = True
         if skip:
-            self.pos = 36
+            self.pos = 53
             self.sourcetree.run_command('git checkout {}'.format(
-                self.sourcetree.get_commit_spec('ch18l020')
+                self.sourcetree.get_commit_spec('ch19l025')
             ))
 
         while self.pos < len(self.listings):
