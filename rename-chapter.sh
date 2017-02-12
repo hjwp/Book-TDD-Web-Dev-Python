9ed898f2a172d42c3460103221126d4e583dd9a5
Harry <hjwp2@cantab.net>
2017-02-12 23:14:34 +0000

    start renaming bdd appendix

--- rename-chapter.sh
+++ rename-chapter.sh
@@ -12,7 +12,7 @@ git mv $OLD_CHAPTER.asciidoc $NEW_NAME.asciidoc
 git mv tests/test_$OLD_CHAPTER.py tests/test_$NEW_NAME.py
 
 mkdir source/$NEW_NAME
-git mv source/$OLD_CHAPTER/superlists source/$NEW_NAME/superlists
+# git mv source/$OLD_CHAPTER/superlists source/$NEW_NAME/superlists
 cd source/$NEW_NAME/superlists && git checkout -b $NEW_NAME && cd ../../..
 
 sed -i s/$OLD_CHAPTER/$NEW_NAME/g .travis.yml atlas.json book.asciidoc copy_html_to_site_and_print_toc.py tests/*.py 
@@ -21,3 +21,4 @@ cd source && ./push-back.sh $NEW_NAME && cd ..
 xvfb-run -a make test_$NEW_NAME || echo -e "\a"
 
 echo git commit -am \"rename $OLD_CHAPTER $NEW_NAME\"
+
