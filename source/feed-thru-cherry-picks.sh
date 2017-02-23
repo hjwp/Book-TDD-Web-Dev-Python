4c0d60e508ee119bd2281128c0763e4aa334923d
Harry <hjwp2@cantab.net>
2017-02-23 12:08:20 +0000

    tweak feed-thru script

--- source/feed-thru-cherry-picks.sh
+++ source/feed-thru-cherry-picks.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-CHAP=$1
-PREV=$2
+PREV=$1
+CHAP=$2
+
 # assumes a git remote local pointing at a local bare repo...
 REPO=local
 
@@ -10,10 +11,11 @@ cd $CHAP/superlists
 
 git checkout $PREV
 git reset --hard $REPO/$PREV
-git checkout $CHAP
 
 git fetch $REPO
+git checkout $CHAP
 git reset --hard $REPO/$PREV
+
 git cherry-pick $PREV..$REPO/$CHAP
 git diff $REPO/$CHAP
 
