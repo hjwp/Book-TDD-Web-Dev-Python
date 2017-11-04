f8cc0567d594c2307e48db2755dfff9b21a3d3f9
Harry <hjwp2@cantab.net>
2017-11-04 13:10:39 +0000

    simplify unit test runner script

--- run_test_tests.sh
+++ run_test_tests.sh
@@ -1,4 +1,3 @@
-export PYTHONHASHSEED=0
-py.test --tb=short tests/test_book_tester.py
+#!/bin/bash
+PYTHONHASHSEED=0 py.test --tb=short tests/test_book_tester.py
 # py.test --tb=short `ls tests/test_* | grep -v test_chapter | grep -v test_server` 
-export PYTHONHASHSEED=
