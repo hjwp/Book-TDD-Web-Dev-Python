468d5ec3136f7230afa00cbe23c7133ead057591
Harry <hjwp2@cantab.net>
2018-01-12 11:12:35 +0000

    test for server-side debugging chap now passing, against real server

--- tests/test_chapter_server_side_debugging.py
+++ tests/test_chapter_server_side_debugging.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
+import os
 import unittest
 import subprocess
 
@@ -9,6 +10,16 @@ class Chapter18Test(ChapterTest):
     chapter_name = 'chapter_server_side_debugging'
     previous_chapter = 'chapter_fixtures_and_wait_decorator'
 
+    def handle_set_email_password(self):
+        listing = self.listings[self.pos]
+        new_line = self.email_password_line.replace('yoursekritpasswordhere', self.actual_password)
+        self.run_server_command(
+            f"sudo sed -i '/SITENAME/ a {new_line}' {listing.filename}"
+        )
+        listing.was_written = True
+        self.pos += 1
+
+
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
 
@@ -18,6 +29,11 @@ class Chapter18Test(ChapterTest):
 
         # skips
         self.skip_with_check(1, "if you haven't already")
+        self.email_password_pos = 12
+        self.email_password_line = "Environment=EMAIL_PASSWORD=yoursekritpasswordhere"
+        self.skip_with_check(self.email_password_pos, self.email_password_line)
+        if DO_SERVER_COMMANDS:
+            self.actual_password = os.environ['EMAIL_PASSWORD']
 
         # prep
         self.start_with_checkout()
@@ -38,7 +54,10 @@ class Chapter18Test(ChapterTest):
 
         while self.pos < len(self.listings):
             print(self.pos)
-            self.recognise_listing_and_process_it()
+            if self.pos == self.email_password_pos:
+                self.handle_set_email_password()
+            else:
+                self.recognise_listing_and_process_it()
 
         self.assert_all_listings_checked(self.listings)
 
