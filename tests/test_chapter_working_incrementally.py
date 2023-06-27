f8dd2d881bf9134f8f127e09ab737909ce278630
Harry <hjwp2@cantab.net>
2023-06-27 22:44:00 +0100

    some small tweaks trynna get tests passing

--- tests/test_chapter_working_incrementally.py
+++ tests/test_chapter_working_incrementally.py
@@ -34,11 +34,11 @@ class Chapter7Test(ChapterTest):
         self.run_command(Command("python3 manage.py migrate --noinput"))
 
         # hack fast-forward
-        skip = False
+        skip = True
         if skip:
-            self.pos = 124
+            self.pos = 19
             self.sourcetree.run_command(
-                "git checkout {}".format(self.sourcetree.get_commit_spec("ch07l041"))
+                "git checkout {}".format(self.sourcetree.get_commit_spec("ch07l009"))
             )
 
         while self.pos < touch_pos:
