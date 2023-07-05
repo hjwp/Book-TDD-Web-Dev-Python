f9da1b993037c9a4d59354de0e88f17554b370ce
Harry <hjwp2@cantab.net>
2023-07-05 11:15:37 +0100

    get all the unit tests passing again

--- run_test_tests.sh
+++ run_test_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PYTHONHASHSEED=0 py.test --tb=short -k 'not test_listings_and_commands_and_output' tests/
+PYTHONHASHSEED=0 py.test --failed-first --tb=short -k 'not test_listings_and_commands_and_output'  $@ tests/
 # py.test --tb=short `ls tests/test_* | grep -v test_chapter | grep -v test_server` 
