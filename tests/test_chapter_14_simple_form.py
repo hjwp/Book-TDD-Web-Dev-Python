2bc575885518d55ff0ca037581352d2348eb0b2c
Harry <hjwp2@cantab.net>
2023-11-08 13:06:38 +0000

    14 almost done

--- tests/test_chapter_14_simple_form.py
+++ tests/test_chapter_14_simple_form.py
@@ -4,20 +4,21 @@ import unittest
 
 from book_tester import ChapterTest
 
+
 class Chapter12Test(ChapterTest):
-    chapter_name = 'chapter_14_simple_form'
-    previous_chapter = 'chapter_13_database_layer_validation'
+    chapter_name = "chapter_14_simple_form"
+    previous_chapter = "chapter_13_database_layer_validation"
 
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing')
-        self.assertEqual(self.listings[1].type, 'code listing')
-        self.assertEqual(self.listings[2].type, 'output')
+        self.assertEqual(self.listings[0].type, "code listing")
+        self.assertEqual(self.listings[1].type, "code listing")
+        self.assertEqual(self.listings[2].type, "output")
 
         # skips
-        self.skip_with_check(29, '# review changes') # diff
+        self.skip_with_check(29, "# review changes")  # diff
 
         # prep
         self.start_with_checkout()
@@ -26,11 +27,13 @@ class Chapter12Test(ChapterTest):
         # hack fast-forward
         skip = True
         if skip:
-            self.pos = 31
-            self.sourcetree.run_command('git checkout {}'.format(
-                self.sourcetree.get_commit_spec('ch11l015')
-            ))
-
+            self.pos = 48  # 31
+            self.sourcetree.run_command(
+                "git checkout {}".format(
+                    # self.sourcetree.get_commit_spec('ch11l015')
+                    self.sourcetree.get_commit_spec("ch14l023")
+                )
+            )
 
         while self.pos < len(self.listings):
             print(self.pos)
@@ -40,5 +43,5 @@ class Chapter12Test(ChapterTest):
         self.check_final_diff(ignore=["moves"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
