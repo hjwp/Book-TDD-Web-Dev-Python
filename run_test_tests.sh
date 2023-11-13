d467675a6fb224131a9f17abf745db1425da6095
Harry <hjwp2@cantab.net>
2023-11-13 11:02:25 +0000

    fix bash bug in run_test_tests

--- run_test_tests.sh
+++ run_test_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-PYTHONHASHSEED=0 py.test --failed-first --tb=short -k 'not test_listings_and_commands_and_output'  $@ tests/
-# py.test --tb=short `ls tests/test_* | grep -v test_chapter | grep -v test_server` 
+PYTHONHASHSEED=0 py.test \
+    --failed-first \
+    --tb=short \
+    -k 'not test_listings_and_commands_and_output' \
+    "$@" \
+    tests/
+# py.test --tb=short `ls tests/test_* | grep -v test_chapter | grep -v test_server`
