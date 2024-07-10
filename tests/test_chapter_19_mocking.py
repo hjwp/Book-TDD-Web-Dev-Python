c834ceb6e81de0b742975804fb199002f57ef107
Harry <hjwp2@cantab.net>
2024-07-10 12:05:55 +0100

    Good progress in 19

--- tests/test_chapter_19_mocking.py
+++ tests/test_chapter_19_mocking.py
@@ -3,32 +3,33 @@ import unittest
 
 from book_tester import ChapterTest
 
+
 class Chapter17Test(ChapterTest):
-    chapter_name = 'chapter_19_mocking'
-    previous_chapter = 'chapter_18_spiking_custom_auth'
+    chapter_name = "chapter_19_mocking"
+    previous_chapter = "chapter_18_spiking_custom_auth"
 
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
         self.start_with_checkout()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing')
-        self.assertEqual(self.listings[1].type, 'code listing')
-        self.assertEqual(self.listings[2].type, 'test')
+        self.assertEqual(self.listings[0].type, "code listing with git ref")
+        self.assertEqual(self.listings[1].type, "code listing with git ref")
+        self.assertEqual(self.listings[2].type, "test")
 
         # skips
-        #self.skip_with_check(22, 'switch back to master') # comment
+        # self.skip_with_check(22, 'switch back to master') # comment
 
         self.prep_database()
-        self.sourcetree.run_command('rm accounts/tests.py')
+        # self.sourcetree.run_command("rm src/accounts/tests.py")
 
         # hack fast-forward
         skip = False
         if skip:
             self.pos = 100
-            self.sourcetree.run_command('git checkout {0}'.format(
-                self.sourcetree.get_commit_spec('ch16l047')
-            ))
+            self.sourcetree.run_command(
+                "git checkout {0}".format(self.sourcetree.get_commit_spec("ch16l047"))
+            )
 
         while self.pos < len(self.listings):
             print(self.pos)
@@ -40,5 +41,5 @@ class Chapter17Test(ChapterTest):
         self.check_final_diff(ignore=["moves"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
