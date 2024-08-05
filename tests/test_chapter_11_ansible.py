14fb50aa1ac977c6edf6118e904e00681a342e1c
Harry <hjwp2@cantab.net>
2024-08-05 12:20:16 +0100

    small start on 11 tests

--- tests/test_chapter_11_ansible.py
+++ tests/test_chapter_11_ansible.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import subprocess
+import os
 import unittest
 
-from book_tester import DO_SERVER_COMMANDS, ChapterTest
+from book_tester import ChapterTest
 
 
-class Chapter9bTest(ChapterTest):
+class Chapter11Test(ChapterTest):
     chapter_name = 'chapter_11_ansible'
     previous_chapter = 'chapter_09_docker'
 
@@ -14,44 +14,28 @@ class Chapter9bTest(ChapterTest):
         self.parse_listings()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'server command')
-        self.assertEqual(self.listings[2].type, 'against staging')
+        self.assertEqual(self.listings[0].type, 'other command')
+        self.assertEqual(self.listings[4].type, 'against staging')
 
         self.start_with_checkout()
-        self.sourcetree.run_command('mkdir -p static/stuff')
+        # self.sourcetree.run_command('mkdir -p static/stuff')
 
         # skips
-        self.skip_with_check(8, 'check our symlink')
-        self.skip_with_check(19, 'Starting gunicorn')
-        self.skip_with_check(41, 'something more secure later')
-        self.skip_with_check(70, 'this command tells Systemd')
-        self.skip_with_check(72, 'this command actually starts')
-        self.skip_with_check(85, 'git status')
-        self.skip_with_check(86, 'see three new files')
+        # self.skip_with_check(8, 'check our symlink')
 
-        # fixes
-        self.replace_command_with_check(
-            39,
-            'git pull',
-            'git fetch'
-            ' && git checkout chapter_11_ansible'
-            ' && git reset --hard origin/chapter_11_ansible',
-        )
-
-        vm_restore = 'MANUAL_END'
+        # vm_restore = 'MANUAL_END'
 
         # hack fast-forward
-        skip = False
-        if skip:
+        if os.environ.get("SKIP"):
             self.pos = 42
             self.sourcetree.run_command('git checkout {0}'.format(
                 self.sourcetree.get_commit_spec('ch08l003')
             ))
 
-        if DO_SERVER_COMMANDS:
-            subprocess.check_call(['vagrant', 'snapshot', 'restore', vm_restore])
-
-        self.current_server_cd = '~/sites/$SITENAME'
+        # if DO_SERVER_COMMANDS:
+        #     subprocess.check_call(['vagrant', 'snapshot', 'restore', vm_restore])
+        #
+        # self.current_server_cd = '~/sites/$SITENAME'
 
         while self.pos < len(self.listings):
             listing = self.listings[self.pos]
@@ -60,10 +44,10 @@ class Chapter9bTest(ChapterTest):
 
         self.assert_all_listings_checked(self.listings)
         self.check_final_diff(ignore=["gunicorn==19"])
-        if DO_SERVER_COMMANDS:
-            subprocess.run(['vagrant', 'snapshot', 'delete', 'MAKING_END'], check=False)
-            subprocess.run(['vagrant', 'snapshot', 'save', 'MAKING_END'], check=True)
-
+        # if DO_SERVER_COMMANDS:
+        #     subprocess.run(['vagrant', 'snapshot', 'delete', 'MAKING_END'], check=False)
+        #     subprocess.run(['vagrant', 'snapshot', 'save', 'MAKING_END'], check=True)
+        #
 
 if __name__ == '__main__':
     unittest.main()
