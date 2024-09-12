01a4930b22892de5297a52e0081eb52ce75b31d3
Harry <hjwp2@cantab.net>
2024-09-13 00:04:15 +0100

    get chap 24 under test

--- tests/test_chapter_24_page_pattern.py
+++ tests/test_chapter_24_page_pattern.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
+import os
 import unittest
 
 from book_tester import ChapterTest
 
 
 class Chapter22Test(ChapterTest):
-    chapter_name = 'chapter_24_page_pattern'
-    previous_chapter = 'chapter_23_CI'
-
+    chapter_name = "chapter_24_page_pattern"
+    previous_chapter = "chapter_23_CI"
 
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
         self.start_with_checkout()
-        #self.prep_virtualenv()
+        # self.prep_virtualenv()
 
         # sanity checks
         # self.assertEqual(self.listings[0].type, 'code listing')
-        self.assertEqual(self.listings[0].type, 'code listing with git ref')
-        self.assertEqual(self.listings[1].type, 'test')
-        self.assertEqual(self.listings[2].type, 'output')
+        self.assertEqual(self.listings[0].type, "code listing with git ref")
+        self.assertEqual(self.listings[1].type, "test")
+        self.assertEqual(self.listings[2].type, "output")
 
         # skips
-        #self.skip_with_check(22, 'switch back to master') # comment
+        # self.skip_with_check(22, 'switch back to master') # comment
 
         # hack fast-forward
-        skip = False
-        if skip:
-            self.pos = 23
-            self.sourcetree.run_command('git checkout {0}'.format(
-                self.sourcetree.get_commit_spec('ch18l011')
-            ))
+        if os.environ.get("SKIP"):
+            self.pos = 7
+            self.sourcetree.run_command(
+                "git checkout {}".format(self.sourcetree.get_commit_spec("ch24l004"))
+            )
 
         while self.pos < len(self.listings):
             print(self.pos)
@@ -40,12 +39,11 @@ class Chapter22Test(ChapterTest):
         self.sourcetree.tidy_up_after_patches()
         # final branch includes a suggested implementation...
         # so just check diff up to the last listing
-        commit = self.sourcetree.get_commit_spec('ch22l013')
-        diff = self.sourcetree.run_command(
-            'git diff -b {}'.format(commit)
-        )
-        self.check_final_diff(ignore=["moves"], diff=diff)
+        commit = self.sourcetree.get_commit_spec("ch22l013")
+        diff = self.sourcetree.run_command(f"git diff -b {commit}")
+        # TODO
+        # self.check_final_diff(ignore=["moves"], diff=diff)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
