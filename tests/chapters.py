15df6fa16ce550b18ca6f60e587beecf95ae0fa6
Harry <hjwp2@cantab.net>
2024-10-10 17:49:58 +0100

    add to chapters.py

--- tests/chapters.py
+++ tests/chapters.py
@@ -21,6 +21,7 @@ CHAPTERS = [
     # part 3
     "chapter_18_spiking_custom_auth",
     "chapter_19_mocking_1",
+    "chapter_20_mocking_2",
     "chapter_20_fixtures_and_wait_decorator",
     "chapter_21_server_side_debugging",
     "chapter_22_outside_in",
