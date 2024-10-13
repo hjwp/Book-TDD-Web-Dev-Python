a7fe5a0075b55660e12e7d44c179ae8f1a5ff6eb
Harry <hjwp2@cantab.net>
2024-10-13 15:32:57 +0100

    fix commit numbers in 14

--- source/fix-commit-numbers.py
+++ source/fix-commit-numbers.py
@@ -6,34 +6,41 @@
 import re
 import sys
 
-incoming = sys.stdin.read()
-if m := re.match(r"(.+) --ch(\d+)l(\d+)(-?)(\d?)--", incoming):
-    prefix, chap_num, listing_num, extra_dash, suffix = m.groups()
-    chap_num = int(chap_num)
-    listing_num = int(listing_num)
-    suffix = int(suffix) if suffix else None
-    if chap_num != 14:
-        chap_num = 14
+while incoming := sys.stdin.readline():
+    # if m := re.match(r"(.+) --ch(\d+)l(\d+)(-?)(\d?)--", incoming.rstrip()):
+    if m := re.match(r"(.+) (\(|--)ch(\d+)l(\d+)(-?)(\d?)(\)|--)", incoming.rstrip()):
+        prefix, sep1, chap_num, listing_num, extra_dash, suffix, sep2 = m.groups()
+        chap_num = int(chap_num)
+        listing_num = int(listing_num)
+        suffix = int(suffix) if suffix else None
+        if chap_num == 14:
+            pass
 
-    if suffix and listing_num == 30:
-        listing_num = listing_num - 1 + suffix
+        elif suffix and listing_num == 30:
+            listing_num = listing_num - 13 + suffix
 
-    if listing_num == 32 and suffix == "1":
-        listing_num = listing_num
-    elif listing_num == 32 and suffix == "2":
-        listing_num = 33
-    elif listing_num == 33 and suffix is None:
-        listing_num = 34
-    elif listing_num == 33 and suffix :
-        listing_num = 35
-    elif listing_num == 34:
-        listing_num = 36
-    elif listing_num == 35:
-        listing_num = 37
-    elif listing_num == 36:
-        assert suffix
-        listing_num = 37 + suffix
+        if listing_num == 30:
+            assert suffix is None
+            listing_num = 21
+        elif listing_num == 31:
+            assert suffix is None
+            listing_num = 22
+        elif listing_num == 32 and suffix == "1":
+            listing_num = 23
+        elif listing_num == 32 and suffix == "2":
+            listing_num = 24
+        elif listing_num == 33 and suffix is None:
+            listing_num = 25
+        elif listing_num == 33 and suffix:
+            listing_num = 26
+        elif listing_num == 34:
+            listing_num = 27
+        elif listing_num == 35:
+            listing_num = 28
+        elif listing_num == 36:
+            assert suffix
+            listing_num = 28 + suffix
 
-    print(f"{prefix} --ch{chap_num}l{listing_num:03d}--")
-else:
-    print(incoming)
+        print(f"{prefix} {sep1}ch{chap_num}l{listing_num:03d}{sep2}")
+    else:
+        print(incoming, end="")
