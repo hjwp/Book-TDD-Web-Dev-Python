7791afb1f0446bed532efb63156d7ae2086306b6
Harry <hjwp2@cantab.net>
2024-10-13 09:49:29 +0100

    wip fixing commit numbers in 14

--- source/fix-commit-numbers.py
+++ source/fix-commit-numbers.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 
-# use with 
+# use with
 # FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter $PWD/../../fix-commit-numbers.py 3fc31f1b..
 
 import re
 import sys
 
 incoming = sys.stdin.read()
-if m := re.match(r"(.+) --ch(\d+)l(\d+)--", incoming):
-    prefix, chap_num, listing_num = m.groups()
-    new_listing_num = int(listing_num) + 1
-    print(f"{prefix} --ch{chap_num}l{new_listing_num:03d}--")
+if m := re.match(r"(.+) --ch(\d+)l(\d+)(-?)(\d?)--", incoming):
+    prefix, chap_num, listing_num, extra_dash, suffix = m.groups()
+    chap_num = int(chap_num)
+    listing_num = int(listing_num)
+    suffix = int(suffix) if suffix else None
+    if chap_num != 14:
+        chap_num = 14
+
+    if suffix and listing_num == 30:
+        listing_num = listing_num - 1 + suffix
+
+    if listing_num == 32 and suffix == "1":
+        listing_num = listing_num
+    elif listing_num == 32 and suffix == "2":
+        listing_num = 33
+    elif listing_num == 33 and suffix is None:
+        listing_num = 34
+    elif listing_num == 33 and suffix :
+        listing_num = 35
+    elif listing_num == 34:
+        listing_num = 36
+    elif listing_num == 35:
+        listing_num = 37
+    elif listing_num == 36:
+        assert suffix
+        listing_num = 37 + suffix
+
+    print(f"{prefix} --ch{chap_num}l{listing_num:03d}--")
 else:
     print(incoming)
