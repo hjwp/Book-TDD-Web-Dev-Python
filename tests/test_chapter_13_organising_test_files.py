b6b800108eaf0da0826f6ee5abb70e4f436f2be6
Harry <hjwp2@cantab.net>
2025-04-11 14:24:31 +0100

    fix 13

--- tests/test_chapter_13_organising_test_files.py
+++ tests/test_chapter_13_organising_test_files.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python3
+import os
 import unittest
 
 from book_tester import ChapterTest
 
 
 class Chapter13Test(ChapterTest):
-    chapter_name = 'chapter_13_organising_test_files'
-    previous_chapter = 'chapter_12_ansible'
+    chapter_name = "chapter_13_organising_test_files"
+    previous_chapter = "chapter_12_ansible"
 
     def test_listings_and_commands_and_output(self):
         self.parse_listings()
 
         # sanity checks
-        self.assertEqual(self.listings[0].type, 'code listing with git ref')
-        self.assertEqual(self.listings[1].type, 'code listing with git ref')
-        self.assertEqual(self.listings[2].type, 'test')
+        self.assertEqual(self.listings[0].type, "code listing with git ref")
+        self.assertEqual(self.listings[1].type, "code listing with git ref")
+        self.assertEqual(self.listings[2].type, "test")
 
         # other prep
         self.start_with_checkout()
         self.prep_database()
 
         # hack fast-forward
-        skip = False
-        if skip:
-            self.pos = 25
-            self.sourcetree.run_command('git switch {}'.format(
-                self.sourcetree.get_commit_spec('ch11l011')
-            ))
+        if os.environ.get("SKIP"):
+            self.pos = 6
+            self.sourcetree.run_command(
+                f"git checkout {self.sourcetree.get_commit_spec('ch13l002')}"
+            )
 
         while self.pos < len(self.listings):
             print(self.pos, self.listings[self.pos].type)
             self.recognise_listing_and_process_it()
 
         self.assert_all_listings_checked(self.listings)
-        self.check_final_diff(ignore=[
-            # "django==1.11"
-        ])
+        self.check_final_diff(
+            ignore=[
+                # "django==1.11"
+            ]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
