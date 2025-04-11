fe58236cbf452d3cf1c5aa45dc20871e2bfe7174
Harry <hjwp2@cantab.net>
2025-04-11 13:17:18 +0100

    Swap order of asserts in 5-7 to avoid a race condition

--- count-todos.py
+++ count-todos.py
@@ -1,15 +1,17 @@
 import csv
+import datetime
 import sys
 from pathlib import Path
 
 MARKERS = ["TODO", "RITA", "DAVID", "SEBASTIAN", "JAN", "CSANAD"]
 
 out = csv.writer(sys.stdout)
-out.writerow(["Chapter"]  + MARKERS)
-for path in sorted(Path(".").rglob("chapter*.asciidoc")):
+out.writerow(["Date", "Chapter"] + MARKERS)
+today = datetime.date.today()
+for path in sorted(
+    list(Path(".").rglob("chapter*.asciidoc"))
+    + list(Path(".").rglob("appendix*.asciidoc"))
+):
     chapter_name = str(path).replace(".asciidoc", "")
-    todos = [
-        path.read_text().count(thing)
-        for thing in MARKERS
-    ]
-    out.writerow([chapter_name] + todos)
+    todos = [path.read_text().count(thing) for thing in MARKERS]
+    out.writerow([today.isoformat(), chapter_name] + todos)
